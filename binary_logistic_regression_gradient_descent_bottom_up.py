8af27fd0e0c77c3acba58f2a92d3963b08ab327b
james-sheridan-code <jamessheridan007@gmail.com>
2026-03-08 10:46:12 +0200

    Refactor gradient descent initialization and logging
    - Updated weight initialization to use float values.
    - Included cost function and printed it per iteration.
    - Fixed spelling of 'descent'
    - Shape (1) corrected to 'scalar'

--- binary_logistic_regression_gradient_descent_bottom_up.py
+++ binary_logistic_regression_gradient_descent_bottom_up.py
@@ -1,12 +1,12 @@
 """
-Manually do Binary Logistic Regression using gradient decent.
+Manually do Binary Logistic Regression using gradient descent.
 
 Steps in Binary Logistic Regression:
 1. Standardise
 (loop over):
 2. y_prediction matrix
 3. get both partial derivatives
-4. replace b and w for gradient decent.
+4. replace b and w for gradient descent.
 """
 
 
@@ -18,7 +18,7 @@ def main():
 
     X_train = standardise(X_train)
 
-    gradient_decent(W=np.array([0,0]), b=0, learn_rate=0.01, iterations=10000, y_train=y_train, X_train=X_train)
+    gradient_descent(W=np.array([0.0,0.0]), b=0, learn_rate=0.01, iterations=10000, y_train=y_train, X_train=X_train)
 
 
 def standardise(variable):
@@ -27,7 +27,7 @@ def standardise(variable):
     return (variable - mean) / std
 
 
-def gradient_decent(W, b, learn_rate, iterations, y_train, X_train):
+def gradient_descent(W, b, learn_rate, iterations, y_train, X_train):
 
     m = len(y_train)
     W = W.reshape(-1,1)
@@ -36,17 +36,20 @@ def gradient_decent(W, b, learn_rate, iterations, y_train, X_train):
     for i in range(iterations):
         # get y predictions
         # Formula: y_pred = 1 / [1 + e^-(X_train @ W + b)]
-        # Shapes: (6,1) = 1 / [1 + e^-{(6,2) @ (2,1) + (1)}]
+        # Shapes: (6,1) = 1 / [1 + e^-{(6,2) @ (2,1) + scalar}]
         y_pred = 1 / (1 + np.exp(-((X_train @ W) + b)))
+
+        # current cost function (to check if cost goes down over time)
+        cost = -np.mean(y_train * np.log(y_pred) + (1 - y_train) * np.log(1 - y_pred))
 
         # get partial derivative of w
         # Formula: dw = (1/m) * X.T @ (y_pred - y_train)
-        # Shapes: (2,1) = (1) * (6,2).T @ ((6,1) - (6,1))
+        # Shapes: (2,1) = scalar * (6,2).T @ ((6,1) - (6,1))
         dw = (1/m) * (X_train.T @ (y_pred - y_train))
         
         # get partial derivative of b
         # Formula: db = (1/m) * Sum(y_pred - y_train)
-        # Shapes: (1) = (1) * (1)
+        # Shapes: scalar = scalar * scalar
         db = (1/m) * np.sum(y_pred - y_train)
 
         temp_W = W - learn_rate * dw
@@ -55,10 +58,11 @@ def gradient_decent(W, b, learn_rate, iterations, y_train, X_train):
         b = temp_b
 
         if i%100 == 0:
-            print(f"Iteration: {i}\tW: {W[0,0]:.2f}, {W[1,0]:.2f}\tb: {b:.2f}")
+            print(f"Iteration: {i}\tCost: {cost:.4f}\tW: {W[0,0]:.2f}, {W[1,0]:.2f}\tb: {b:.2f}")
 
     print(f"Final Results:\nw: {W[0,0]:.2f}, {W[1,0]:.2f}\nb: {b:.2f}")
 
 
 if __name__ == '__main__':
+
     main()
